280a3c0bab4f3c39ebaadb7decd8db000aca56fe
Pierluigi <mail@pierluigi.me>
2019-05-07 17:07:00 +0200

    Drop Async_extended dependency

--- hera/logging/logging.ml
+++ hera/logging/logging.ml
@@ -1,12 +1,11 @@
 open Async
-open Async_extended
 module Http = Log.Make_global ()
 module Module = Log.Make_global ()
 module Main = Log.Make_global ()
 
 let () =
-  Main.set_output [Extended_log.Console.output ~debug:[`Dim] ~info:[`Bright] (Lazy.force Writer.stderr)];
-  Module.set_output [Extended_log.Console.output ~debug:[`Dim] (Lazy.force Writer.stderr)];
-  Http.set_output [Extended_log.Console.output ~debug:[`Dim] (Lazy.force Writer.stderr)];
+  Main.set_output [Log_extended.Console.output ~debug:[`Dim] ~info:[`Bright] (Lazy.force Writer.stderr)];
+  Module.set_output [Log_extended.Console.output ~debug:[`Dim] (Lazy.force Writer.stderr)];
+  Http.set_output [Log_extended.Console.output ~debug:[`Dim] (Lazy.force Writer.stderr)];
   Http.set_level `Debug
 ;;
